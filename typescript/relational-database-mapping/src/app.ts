12d1e32946f0c67136b5576fc7b5ed0fdfe438f0
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2024-07-26 13:08:25 +0900

    use close method + async/await

--- typescript/relational-database-mapping/src/app.ts
+++ typescript/relational-database-mapping/src/app.ts
@@ -10,7 +10,9 @@ const soccerPlayer: Soccer = new Soccer("김축구", "축구명문", "Soccer");
 const criketPlayer: Criket = new Criket("김크켓", 0.67, "Criket");
 const bowlingPlayer: Bowling = new Bowling("김볼링", 0.71, 100, "Bowling");
 
-RdbmsConfig.insertAthlete(soccerPlayer);
-RdbmsConfig.insertAthlete(criketPlayer);
-RdbmsConfig.insertAthlete(bowlingPlayer);
-RdbmsConfig.getAthlete();
+await RdbmsConfig.insertAthlete(soccerPlayer);
+await RdbmsConfig.insertAthlete(criketPlayer);
+await RdbmsConfig.insertAthlete(bowlingPlayer);
+await RdbmsConfig.getAthlete();
+
+RdbmsConfig.close();
